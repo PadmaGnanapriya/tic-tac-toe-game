949268d40fd21b25c121bb5e5fea4887d7b9ab66
PadmaGnanapriya <padmaisuru@gmail.com>
2020-10-10 15:32:33 +0530

    Create switching "X" and "O" with click.

--- src/Board.js
+++ src/Board.js
@@ -6,15 +6,25 @@ class Board extends React.Component {
         super(props);
         this.state = {
             squares: Array(9).fill(null),
+            xIsNext:true,
         };
     }
 
+    handleClick(i){
+        const squares =this.state.squares.slice();
+        squares[i] =this.state.xIsNext ? 'X': 'O';
+        this.setState({
+            squares:squares,
+            xIsNext:!this.state.xIsNext,
+        });
+    }
+
     renderSquare(i) {
-        return <Square value={this.state.squares[i]} />;
+        return <Square value={this.state.squares[i]} onClick={()=> this.handleClick(i)}/>;
     }
 
     render() {
-        const status = 'Next player: X';
+        const status = 'Next player: ' +(this.state.xIsNext ? 'X' : 'O');
 
         return (
             <div>
--- src/Square.js
+++ src/Square.js
@@ -1,19 +1,12 @@
 import React from "react";
 
-class Square extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            value: null,
-        };
-    }
-    render() {
+function Square(props) {
         return (
-            <button className="square" onClick={() => this.setState({value: 'X'})}>
-                {this.state.value}
+            <button className="square" onClick={props.onClick }>
+                {props.value}
             </button>
         );
-    }
+
 }
 
 export default Square;
